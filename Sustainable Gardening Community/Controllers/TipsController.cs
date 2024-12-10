3d0524efc7ed5975e5a010ad63deed807ab716d7
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2024-12-11 00:42:46 +0600

    Home and About page completed

---
+++ Sustainable Gardening Community/Controllers/TipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sustainable_Gardening_Community.Controllers
+{
+    public class TipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
